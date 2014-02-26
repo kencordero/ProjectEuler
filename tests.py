7e0adb7eb9eec6ea60e49e60ff4e1757a8e0c9f0
Kenneth Cordero <kencordero@gmail.com>
2014-02-26 09:28:09 -0600

    More aliases

--- tests.py
+++ tests.py
@@ -3,55 +3,66 @@ import unittest
 
 class TestEuler(unittest.TestCase):
     def test_001(self):
-        self.assertEqual(Problems.problem_001(2, 3, 10), 32)
-        self.assertEqual(Problems.problem_001(3, 5, 9), 14)
-        self.assertEqual(Problems.problem_001(3, 5, 10), 23)
+        f = Problems._001
+        self.assertEqual(f(2, 3, 10), 32)
+        self.assertEqual(f(3, 5, 9), 14)
+        self.assertEqual(f(3, 5, 10), 23)
         
     def test_001_alt(self):
-        self.assertEqual(Problems.problem_001_alt(2, 3, 10), 32)
-        self.assertEqual(Problems.problem_001_alt(3, 5, 9), 14)
-        self.assertEqual(Problems.problem_001_alt(3, 5, 10), 23)
+        f = Problems._001_alt
+        self.assertEqual(f(2, 3, 10), 32)
+        self.assertEqual(f(3, 5, 9), 14)
+        self.assertEqual(f(3, 5, 10), 23)
     
     def test_002(self):
-        self.assertEqual(Problems.problem_002(10), 10)
-        self.assertEqual(Problems.problem_002(100), 44)
+        f = Problems._002
+        self.assertEqual(f(10), 10)
+        self.assertEqual(f(100), 44)
         
     def test_003(self):
-        self.assertEqual(Problems.problem_003(28), 7)
-        self.assertEqual(Problems.problem_003(29), 29)
-        self.assertEqual(Problems.problem_003(100), 5)
-        self.assertEqual(Problems.problem_003(13195), 29)
+        f = Problems._003
+        self.assertEqual(f(28), 7)
+        self.assertEqual(f(29), 29)
+        self.assertEqual(f(100), 5)
+        self.assertEqual(f(13195), 29)
     
     def test_004(self):
-        self.assertEqual(Problems.problem_004(1), 9)
-        self.assertEqual(Problems.problem_004(2), 9009)        
+        f = Problems._004
+        self.assertEqual(f(1), 9)
+        self.assertEqual(f(2), 9009)        
         
     def test_005(self):
-        self.assertEqual(Problems.problem_005(5), 60)
-        self.assertEqual(Problems.problem_005(10), 2520)
+        f = Problems._005
+        self.assertEqual(f(5), 60)
+        self.assertEqual(f(10), 2520)
         
     def test_006(self):
-        self.assertEqual(Problems.problem_006(1), 1)       
-        self.assertEqual(Problems.problem_006(10), 2640)
+        f = Problems._006
+        self.assertEqual(f(1), 1)       
+        self.assertEqual(f(10), 2640)
         
     def test_007(self):
-        self.assertEqual(Problems.problem_007(1), 2)
-        self.assertEqual(Problems.problem_007(2), 3)
-        self.assertEqual(Problems.problem_007(3), 5)
-        self.assertEqual(Problems.problem_007(4), 7)
-        self.assertEqual(Problems.problem_007(5), 11)
-        self.assertEqual(Problems.problem_007(6), 13)
+        f = Problems._007
+        self.assertEqual(f(1), 2)
+        self.assertEqual(f(2), 3)
+        self.assertEqual(f(3), 5)
+        self.assertEqual(f(4), 7)
+        self.assertEqual(f(5), 11)
+        self.assertEqual(f(6), 13)
         
     def test_008(self):
-        self.assertEqual(Problems.problem_008(1, 1), 1)
+        f = Problems._008
+        self.assertEqual(f(1, 1), 1)
         
     def test_009(self):
-        self.assertEqual(Problems.problem_009(12), 60)
-        self.assertEqual(Problems.problem_009(30), 780)        
+        f = Problems._009
+        self.assertEqual(f(12), 60)
+        self.assertEqual(f(30), 780)        
         
     def test_010(self):
-        self.assertEqual(Problems.problem_010(10), 17)
-        self.assertEqual(Problems.problem_010(20), 77)
+        f = Problems._010
+        self.assertEqual(f(10), 17)
+        self.assertEqual(f(20), 77)
         
 if __name__ == '__main__':
     unittest.main()
