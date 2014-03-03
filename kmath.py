40e465818b10dd25da3b70818b9f95b821233204
Kenneth Cordero <kencordero@gmail.com>
2014-03-03 09:02:11 -0600

    reordered methods

--- kmath.py
+++ kmath.py
@@ -1,4 +1,14 @@
+import math
+import re
 #useful math functions
+
+PHI = (1 + pow(5, 0.5)) / 2
+
+def factorial(x):
+    if not float(x).is_integer() or x < 0:
+        raise ValueError
+    return 1 if x < 2 else x * factorial(x - 1)	
+    
 def fibonacci_gen():
     a, b = 1, 0
     while True:
@@ -6,14 +16,13 @@ def fibonacci_gen():
         b += a
         yield b
         a += b
-        
-def get_nth_fibonacci(n):
-    if not float(n).is_integer() or n < 1:
-        raise ValueError
-    root5 = pow(5, 0.5)
-    ratio = (1 + root5) / 2
-    return round((pow(ratio, n) - pow(1 - ratio, n)) / root5)
 
+def GCF(x, y):
+    if (y > x):  #swap the numbers
+        x, y = y, x 
+    remainder = x % y
+    return y if remainder == 0 else GCF(y, remainder)
+    
 def get_largest_prime_factor(x):
     lpf = 0 #largest prime factor
     i = 2
@@ -24,36 +33,42 @@ def get_largest_prime_factor(x):
         i += 1
     return lpf
     
+def get_n_digits(x): # doesn't handle non-integers
+    if x == 0:
+        return 1
+    return int(math.log10(abs(x))) + 1
+    
+def get_nth_fibonacci(n):
+    if not float(n).is_integer() or n < 1:
+        raise ValueError
+    root5 = pow(5, 0.5)
+    ratio = (1 + root5) / 2
+    return round((pow(ratio, n) - pow(1 - ratio, n)) / root5)
+
 def get_smallest_prime_factor(x):
     i = 2
     while i <= x:
         if x % i == 0:
             return i
         i += 1
+
+def is_palindrome(x):
+    return str(x) == str(x)[::-1]
     
-def factorial(x):
-    if not float(x).is_integer() or x < 0:
-        raise ValueError
-    elif x < 2:
-        return 1
-    else:
-        return x * factorial(x - 1)	
+def is_phone_number(x):
+    phone_pattern = (r'''
+                # don't match beginning of string, number can start anywhere
+    (\d{3})     # area code is 3 digits (e.g. '800')
+    \D*         # optional separator is any number of non-digits
+    (\d{3})     # trunk is 3 digits (e.g. '555')
+    \D*         # optional separator
+    (\d{4})     # rest of number is 4 digits (e.g. '1212')
+    \D*         # optional separator
+    (\d*)       # extension is optional and can be any number of digits
+    $           # end of string
+    ''')
+    return re.search(phone_pattern, x, re.VERBOSE)
 
-def GCF(x, y):
-    if (y > x):  #swap the numbers
-        x, y = y, x 
-    remainder = x % y
-    if (remainder == 0):
-        return y
-    else:
-        return GCF(y, remainder)
-
-def LCM(x, y):
-    return int(x * y / GCF(x, y))
-
-def PHI(): #golden ratio    
-    return (1 + pow(5, 0.5)) / 2
-    
 def is_prime(x):
     if x < 0 or not float(x).is_integer():
         raise ValueError
@@ -62,16 +77,50 @@ def is_prime(x):
     for i in range(2, int(pow(x, 0.5)) + 1):
         if x % i == 0:
             return False        
-    return True    
+    return True
+    
+def is_roman(x):
+    pattern = '''
+        ^                   # beginning of string
+        M{0,3}              # thousands - 0 to 3 Ms
+        (CM|CD|D?C{0,3})    # hundreds - 900 (CM), 400 (CD), 0-300 (0 to 3 Cs),
+                            #            or 500-800 (D, followed by 0 to 3 Cs)
+        (XC|XL|L?X{0,3})    # tens - 90 (XC), 40 (XL), 0-30 (0 to 3 Xs),
+                            #        or 50-80 (L, followed by 0 to 3 Xs)
+        (IX|IV|V?I{0,3})    # ones - 9 (IX), 4 (IV), 0-3 (0 to 3 Is),
+                            #        or 5-8 (V, followed by 0 to 3 Is)
+        $                   # end of string
+        '''
+    return re.search(pattern, x, re.VERBOSE)
 
-def is_palindrome(x):
-    return str(x) == str(x)[::-1]
-
+def LCM(x, y):
+    return int(x * y / GCF(x, y))
+    
 def reduce_fraction(numerator, denominator):
     gcf = GCF(numerator, denominator)
     numerator = numerator / gcf
     denominator = denominator / gcf
     return numerator, denominator
     
-def convert_to_roman(x):
-    pass
+def to_roman(x):
+    if 0 >= x < 4000:
+        raise ValueError
+    roman = (( 'M' , 1000),
+             ('CM',   900),
+             ( 'D' ,  500),
+             ('CD',   400),
+             ( 'C' ,  100),
+             ('XC',    90),
+             ( 'L' ,   50),
+             ('XL',    40),
+             ( 'X' ,   10),
+             ('IX',     9),
+             ( 'V' ,    5),
+             ('IV',     4),
+             ( 'I' ,    1))
+    output = ''
+    for key, value in roman:
+        while x >= value:
+            output += key
+            x -= value
+    return output
--- tests_kmath.py
+++ tests_kmath.py
@@ -2,34 +2,6 @@ import unittest
 import kmath
 
 class TestKMath(unittest.TestCase):
-    def test_fibonacci_gen(self):
-        gen = kmath.fibonacci_gen()
-        self.assertEqual(next(gen), 1)
-        self.assertEqual(next(gen), 1)
-        self.assertEqual(next(gen), 2)
-        self.assertEqual(next(gen), 3)
-        self.assertEqual(next(gen), 5)
-        self.assertEqual(next(gen), 8)
-        self.assertEqual(next(gen), 13)
-        self.assertEqual(next(gen), 21)
-        self.assertEqual(next(gen), 34)
-        self.assertEqual(next(gen), 55)
-        
-    def test_get_nth_fibonacci(self):
-        f = kmath.get_nth_fibonacci
-        self.assertRaises(ValueError, f, 0)
-        self.assertEqual(f(1), 1)
-        self.assertEqual(f(2), 1)
-        self.assertEqual(f(3), 2)
-        self.assertEqual(f(4), 3)
-        self.assertRaises(ValueError, f, 4.5)
-        self.assertEqual(f(5), 5)
-        self.assertEqual(f(6), 8)
-        self.assertEqual(f(7), 13)
-        self.assertEqual(f(8), 21)
-        self.assertEqual(f(9), 34)
-        self.assertEqual(f(10), 55)
-    
     def test_factorial(self):
         f = kmath.factorial
         self.assertRaises(ValueError, f, -1)
@@ -45,24 +17,89 @@ class TestKMath(unittest.TestCase):
         self.assertEqual(f(8), 40320)
         self.assertEqual(f(9), 362880)
         self.assertEqual(f(10), 3628800)
-	
+        
+    def test_fibonacci_gen(self):
+        gen = kmath.fibonacci_gen()
+        self.assertEqual(next(gen), 1)
+        self.assertEqual(next(gen), 1)
+        self.assertEqual(next(gen), 2)
+        self.assertEqual(next(gen), 3)
+        self.assertEqual(next(gen), 5)
+        self.assertEqual(next(gen), 8)
+        self.assertEqual(next(gen), 13)
+        self.assertEqual(next(gen), 21)
+        self.assertEqual(next(gen), 34)
+        self.assertEqual(next(gen), 55)
+    
     def test_GCF(self):
         f = kmath.GCF
         self.assertEqual(f(1920, 1080), 120)
         self.assertEqual(f(120, 240), 120)
         self.assertEqual(f(50, 60), 10)
         self.assertEqual(f(10, 5), 5)
+    
+    def test_get_largest_prime_factor(self):
+        f = kmath.get_largest_prime_factor
+        self.assertEqual(f(28), 7)
+        self.assertEqual(f(29), 29)
+        self.assertEqual(f(100), 5)
+        self.assertEqual(f(13195), 29)
+       
+    def test_get_n_digits(self):
+        f = kmath.get_n_digits
+        self.assertEqual(f(-100), 3)
+        self.assertEqual(f(0), 1)
+        self.assertEqual(f(99), 2)    
         
-    def test_LCM(self):
-        f = kmath.LCM
-        self.assertEqual(f(3, 4), 12)
-        self.assertEqual(f(4, 6), 12)
-        self.assertEqual(f(6, 8), 24)
-        self.assertEqual(f(8, 12), 24)
-        self.assertEqual(f(12, 15), 60)
-        self.assertEqual(f(15, 20), 60)
-        self.assertEqual(f(20, 30), 60)
-        
+    def test_get_nth_fibonacci(self):
+        f = kmath.get_nth_fibonacci
+        self.assertRaises(ValueError, f, 0)
+        self.assertEqual(f(1), 1)
+        self.assertEqual(f(2), 1)
+        self.assertEqual(f(3), 2)
+        self.assertEqual(f(4), 3)
+        self.assertRaises(ValueError, f, 4.5)
+        self.assertEqual(f(5), 5)
+        self.assertEqual(f(6), 8)
+        self.assertEqual(f(7), 13)
+        self.assertEqual(f(8), 21)
+        self.assertEqual(f(9), 34)
+        self.assertEqual(f(10), 55)
+ 
+    def test_get_smallest_prime_factor(self):
+        f = kmath.get_smallest_prime_factor
+        self.assertEqual(f(28), 2)
+        self.assertEqual(f(29), 29)
+        self.assertEqual(f(100), 2)
+        self.assertEqual(f(13195), 5)
+     
+    def test_is_palindrome(self):
+        f = kmath.is_palindrome
+        self.assertFalse(f(-1))
+        self.assertTrue(f(0))
+        self.assertTrue(f(4.4)) 
+        self.assertFalse(f(4.5))
+        self.assertTrue(f(5))
+        self.assertFalse(f(15))
+        self.assertTrue(f(66))
+        self.assertTrue(f(232))
+        self.assertTrue(f(1001))
+        self.assertFalse(f(1031))
+    
+    def test_is_phone_number(self):
+        test_cases = ('800-555-1212',
+                      '800 555 1212',
+                      '800.555.1212',
+                      '(800) 555-1212',
+                      '1-800-555-1212',
+                      '80055512121234',
+                      '800-555-1212-1234',
+                      '800-555-1212x1234',
+                      '800-555-1212 ext. 1234',
+                      'work 1-(800) 555.1212 #1234')
+        for test_case in test_cases:
+            self.assertTrue(kmath.is_phone_number(test_case))
+
     def test_is_prime(self):
         f = kmath.is_prime
         self.assertRaises(ValueError, f, -1)
@@ -78,43 +115,31 @@ class TestKMath(unittest.TestCase):
         self.assertFalse(f(8))
         self.assertFalse(f(9))
         self.assertFalse(f(10))
-        self.assertTrue(f(11))    
+        self.assertTrue(f(11)) 
         
-    def test_is_palindrome(self):
-        f = kmath.is_palindrome
-        self.assertFalse(f(-1))
-        self.assertTrue(f(0))
-        self.assertTrue(f(4.4)) 
-        self.assertFalse(f(4.5))
-        self.assertTrue(f(5))
-        self.assertFalse(f(15))
-        self.assertTrue(f(66))
-        self.assertTrue(f(232))
-        self.assertTrue(f(1001))
-        self.assertFalse(f(1031))
+    def test_is_roman(self):
+        f = kmath.is_roman
+        self.assertFalse(f('MMDMCLIXV'))
+        self.assertTrue(f('MMMDCCCLXXXVIII'))
         
+    def test_LCM(self):
+        f = kmath.LCM
+        self.assertEqual(f(3, 4), 12)
+        self.assertEqual(f(4, 6), 12)
+        self.assertEqual(f(6, 8), 24)
+        self.assertEqual(f(8, 12), 24)
+        self.assertEqual(f(12, 15), 60)
+        self.assertEqual(f(15, 20), 60)
+        self.assertEqual(f(20, 30), 60)
+                          
     def test_reduce_fraction(self):
         f = kmath.reduce_fraction
         self.assertEqual(f(1920, 1080), (16, 9))
         self.assertEqual(f(120, 240), (1, 2))
         self.assertEqual(f(50, 60), (5, 6))
         self.assertEqual(f(10, 5), (2, 1))
-    
-    def test_get_largest_prime_factor(self):
-        f = kmath.get_largest_prime_factor
-        self.assertEqual(f(28), 7)
-        self.assertEqual(f(29), 29)
-        self.assertEqual(f(100), 5)
-        self.assertEqual(f(13195), 29)
-        
-    def test_get_smallest_prime_factor(self):
-        f = kmath.get_smallest_prime_factor
-        self.assertEqual(f(28), 2)
-        self.assertEqual(f(29), 29)
-        self.assertEqual(f(100), 2)
-        self.assertEqual(f(13195), 5)
-        
-    def test_convert_to_roman(self):
+                   
+    def test_to_roman(self):
         test_cases = ((1, 'I'),
                      (2, 'II'),
                      (3, 'III'),
@@ -125,20 +150,20 @@ class TestKMath(unittest.TestCase):
                      (8, 'VIII'),
                      (9, 'IX'),
                      (10, 'X'),
+                     (31, 'XXXI'),
                      (50, 'L'),
                      (100, 'C'),
-                     (500, 'D'),
-                     (1000, 'M'),
-                     (31, 'XXXI'),
                      (148, 'CXLVIII'),
                      (294, 'CCXCIV'),
                      (312, 'CCCXII'),
                      (421, 'CDXXI'),
+                     (500, 'D'),
                      (528, 'DXXVIII'),
                      (621, 'DCXXI'),
                      (782, 'DCCLXXXII'),
                      (870, 'DCCCLXX'),
                      (941, 'CMXLI'),
+                     (1000, 'M'),
                      (1043, 'MXLIII'),
                      (1110, 'MCX'),
                      (1226, 'MCCXXVI'),
@@ -172,7 +197,7 @@ class TestKMath(unittest.TestCase):
                      (3940, 'MMMCMXL'),
                      (3999, 'MMMCMXCIX'))
         for integer, numeral in test_cases:
-            self.assertEqual(kmath.convert_to_roman(integer), numeral)
+            self.assertEqual(kmath.to_roman(integer), numeral)            
         
 if __name__ == '__main__':
     unittest.main()
